b6e377110438b2f65b02579c764438323b77518a
root <root@ip-172-31-37-46.us-east-2.compute.internal>
2022-06-15 03:50:27 +0000

    first commit

---
+++ src/main/java/Wells Fargo/App.java
@@ -0,0 +1,13 @@
+package Wells Fargo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Wells Fargo/AppTest.java
@@ -0,0 +1,20 @@
+package Wells Fargo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
